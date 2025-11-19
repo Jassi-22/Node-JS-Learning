b662447ab4733b2b30d5b49ede64aedbc323b811
Jassi Singh <Singhjassi1903@gmail.com>
2025-11-20 01:35:45 +0530

    DataType Conversion

---
+++ 01_Basics/02_conversion.js
@@ -0,0 +1,21 @@
+
+let name  = "practice"
+let numbername = Number(name)
+console.log(numbername) // NaN
+console.log(typeof numbername) //NUMBER
+
+let age  = 10
+let stringage = String(age) 
+let booleanAge = Boolean(age) 
+let nummAge = null
+console.log(stringage) // 10
+console.log(typeof stringage) //string
+
+console.log(booleanAge) // true
+console.log(typeof booleanAge) // boolean
+
+console.log(nummAge) // null
+console.log(typeof nummAge) // object
+
+
+
