b19e0826f0dc65c4522daf745ee8c8f170785fad
Jassi Singh <Singhjassi1903@gmail.com>
2025-11-20 00:43:52 +0530

    Variable and dataTypes

---
+++ 01_Basics/01_dataTypes.js
@@ -0,0 +1,7 @@
+let name = "example"
+let age = 18
+let pass = true
+let address = null
+let anything = ""
+
+console.table([typeof name, typeof age, typeof pass,typeof address,typeof anything])
---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = "12344"
+let state = "jaipur"
+var name = "abcd"
+address = "jaipur rajasthan"
+
+let correctId
+state = "sanganer"
+name = "xcvbnm"
+address = "rajasthan"
+console.log(accountId)
+console.table([accountId,state,name,address,correctId])
